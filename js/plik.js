b4bc96df853d610f2e3c4bff82770be6839ee5d6
SankHere <a.ksiezopolski1@o2.pl>
2018-09-25 20:13:18 +0200

    Zaczeta obsługa początku i końca gry. Poprawa odbijania od rakietki.

--- js/plik.js
+++ js/plik.js
@@ -18,19 +18,39 @@ const paddleHeight = 10;
 const paddleWidth = 100;
 
 let playerX = 200 //do zmiany
-const playerY = 550;
+const playerY = 560;
 
 //przeszkody
 const blockHeight = 35;
 const blockWidth = 35;
 
 //predkosc pileczki
-let ballSpeedX = -2.7; //1,2
-let ballSpeedY = -3.7; //-1,2
+let ballSpeedX = -6; //1,2
+let ballSpeedY = -6; //-1,2
 
 //tablica przeszkod lvl I
-const przeszkodyX = [85, 135, 185, 235, 285, 335, 385, 435, 485, 535, 585, 635, 685, 735, 785, 835, 885];
-const przeszkodyY = [70, 120, 170];
+let przeszkodaXY = [];
+
+przeszkodaXY.push({
+    x: 100,
+    y: 60
+});
+przeszkodaXY.push({
+    x: 140,
+    y: 60
+});
+przeszkodaXY.push({
+    x: 180,
+    y: 60
+});
+przeszkodaXY.push({
+    x: 180,
+    y: 100
+});
+przeszkodaXY.push({
+    x: 140,
+    y: 100
+});
 
 //f rysujaca deske
 function player() {
@@ -43,11 +63,10 @@ function table() {
     ctx.fillRect(0, 0, cw, ch);
 
     //rysuje bloczki do zbicia
-    for (let i = 0; i < przeszkodyY.length; i++) {
-        for (let j = 0; j < przeszkodyX.length; j++) {
-            ctx.fillStyle = "blue";
-            ctx.fillRect(przeszkodyX[j], przeszkodyY[i], blockHeight, blockHeight);
-        }
+    for (let i = 0; i < przeszkodaXY.length; i++) {
+        //console.log(przeszkodaXY[i].x);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(przeszkodaXY[i].x, przeszkodaXY[i].y, blockHeight, blockHeight);
     }
 }
 
@@ -62,42 +81,77 @@ function ball() {
     ballY = ballY + ballSpeedY;
 
     //odbijanie piłki
-    if (ballY <= 0 || ballY + ballSize >= ch) {
+    //od gora
+    if (ballY <= 0) {
         ballSpeedY = -ballSpeedY;
+        speedUp();
     }
+    //od deski
+    //    if (((ballY + ballSize >= playerY || ballY + ballSize >= playerY + paddleWidth) && ballX > playerX && ballX + ballSize <= playerX + paddleWidth)) {
+    //
+    //    }
 
+    if (ballY + ballSize >= playerY && ballY + ballSize <= playerY + paddleWidth && ballX >= playerX - ballSize + 5 && ballX + ballSize <= playerX + paddleWidth + ballSize - 5) {
+        if (ballY > playerY) {
+            console.log('koniec');
+            clearInterval(help);
+        } else {
+            console.log('tutaj');
+            ballSpeedY = -ballSpeedY;
+            speedUp();
+        }
+    }
+    //lewo prawo
     if (ballX <= 0 || ballX + ballSize >= cw) {
         ballSpeedX = -ballSpeedX;
+        speedUp();
     }
-
+    //koniec gry
+    if (ballY >= ch) {
+        console.log('koniec');
+        clearInterval(help);
+    }
     //odbijanie przeszkody
-    for (let i = 0; i < przeszkodyY.length; i++) {
-        for (let j = 0; j < przeszkodyX.length; j++) {
-
-            if (
-                ballY <= przeszkodyY[i] + blockHeight &&
-                ballY + ballSize >= przeszkodyY[i] &&
-                ballX + ballSize >= przeszkodyX[j] &&
-                ballX <= przeszkodyX[j] + blockWidth
-            ) {
-
-                //console.log('hey');
-                ballSpeedY = -ballSpeedY;
-            }
-
-            if (
-                ballX + ballSize >= przeszkodyX[j] &&
-                ballX <= przeszkodyX[j] + blockWidth &&
-                ballY <= przeszkodyY[i] &&
-                ballY + ballSize >= przeszkodyY[i]
-            ) {
-                //console.log('hey2');
-                ballSpeedX = -ballSpeedX;
-            }
+    for (let i = 0; i < przeszkodaXY.length; i++) {
+        //odbijanie góra/dół
+        if (
+            ballY <= przeszkodaXY[i].y + blockHeight &&
+            ballY + ballSize >= przeszkodaXY[i].y &&
+            ballX + ballSize >= przeszkodaXY[i].x &&
+            ballX <= przeszkodaXY[i].x + blockWidth
+        ) {
+            //console.log('hey');
+            ballSpeedY = -ballSpeedY;
+            przeszkodaXY.splice(i, 1);
         }
+        //odbijanie lewa/prawa
+        //            if (
+        //                ballX + ballSize >= przeszkodyX[j] &&
+        //                ballX <= przeszkodyX[j] + blockWidth &&
+        //                ballY <= przeszkodyY[i] &&
+        //                ballY + ballSize >= przeszkodyY[i]
+        //            ) {
+        //                //console.log('hey2');
+        //                ballSpeedY = -ballSpeedY;
+        //            }
     }
 }
 
+//f przyspieszenie pilki
+function speedUp() {
+    //po X
+    if (ballSpeedX > 0 && ballSpeedX < 16) {
+        ballSpeedX += .1;
+    } else if (ballSpeedX >= 0 && ballSpeedX < 16) {
+        ballSpeedX -= .1;
+    }
+    //po Y
+    if (ballSpeedY > 0 && ballSpeedY < 16) {
+        ballSpeedY += .1;
+    } else if (ballSpeedY >= 0 && ballSpeedY < 16) {
+        ballSpeedY -= .1;
+    }
+}
 //Ruch rakietki
 leftCanvas = canvas.offsetLeft;
 //console.log(leftCanvas);
@@ -120,6 +174,11 @@ function game() {
     table();
     ball();
     player();
+
+    if (przeszkodaXY.length == 0) {
+        console.log('wygrales');
+        clearInterval(help);
+    }
 }
 
-setInterval(game, 1000 / 60);
+let help = setInterval(game, 1000 / 60);
